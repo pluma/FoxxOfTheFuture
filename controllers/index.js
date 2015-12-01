5bf27d0dbc47a52b840002689bb986274d2f5f55
Alan Plum <me@pluma.io>
2015-12-01 19:06:11 +0100

    Update and rename index.js to controllers/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const context = require('@foxx/context'); // magic
-const helloWorldRouter = require('./controllers/hello-world');
-const todosRouter = require('./controllers/todos');
+const helloWorldRouter = require('./hello-world');
+const todosRouter = require('./todos');
 
 context.mount('/todos', todosRouter);
 context.mount('/', helloWorldRouter);
