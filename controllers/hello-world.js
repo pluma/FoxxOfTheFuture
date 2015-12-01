6c3c05091be5e28af09e627e51c5950e6e275cee
Alan Plum <me@pluma.io>
2015-12-01 17:17:40 +0100

    Create hello-world.js

---
+++ controllers/hello-world.js
@@ -0,0 +1,18 @@
+const context = require('@foxx/context'); // magic
+const createRouter = require('@foxx/router');
+const routes = createRouter();
+
+routes.get('/:name', (req, res) => {
+  res.send(`Hello ${req.pathParams.name}`);
+}, 'hello') // route names are context-specific
+.description(
+  'Hello world'
+);
+
+routes.get('/', (req, res) => {
+  const helloWorldUrl = context.route('hello', {name: 'World'});
+  res.redirect(helloWorldUrl);
+})
+.description(
+  'Redirect to Hello World route'
+);
