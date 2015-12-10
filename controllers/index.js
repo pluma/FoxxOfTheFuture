290d16d36b2859edadcfeb86d19944b701a7ca1c
Alan Plum <me@pluma.io>
2015-12-10 19:02:36 +0100

    Add session middleware

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,18 @@
 const context = require('@foxx/context'); // magic
+const sessionsMiddleware = require('@foxx/sessions'); // replaces activateSessions
+const jwtStorage = require('@foxx/sessions/jwt'); // not a Foxx app
 const helloWorldRouter = require('./hello-world');
 const todosRouter = require('./todos');
+
+// Middleware can be used on the context directly, too
+context.use(sessionsMiddleware({
+  sessionStorage: jwtStorage({
+    secret: 'keyboardcat',
+    type: 'HS512'
+  }),
+  header: 'x-session-id',
+  expiry: 60 // minutes
+}));
 
 context.mount('/todos', todosRouter);
 context.mount('/', helloWorldRouter);
