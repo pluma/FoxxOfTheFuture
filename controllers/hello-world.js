ff68359c5fe8325f610a3ba0d322d033801184ed
Alan Plum <me@pluma.io>
2015-12-10 19:15:34 +0100

    Magical comments example
    Caveat: magical comments would need to be re-implemented differently. The current way is broken for comments in unexpected places (e.g. inside object literals). Maybe the comments can be saved to the module/context directly at parse time rather than at runtime?

--- controllers/hello-world.js
+++ controllers/hello-world.js
@@ -3,17 +3,18 @@ const createRouter = require('@foxx/router');
 const routes = createRouter();
 module.exports = routes;
 
+/**
+ * Hello world
+ */ // magical comments are problematic tho
 routes.get('/:name', (req, res) => {
   res.send(`Hello ${req.pathParams.name}`);
-}, 'hello') // route names are context-specific
-.description(
-  'Hello world'
-);
+}, 'hello'); // route names are context-specific;
 
+/**
+ * Redirect to Hello World route
+ */
 routes.get('/', (req, res) => {
   const helloWorldUrl = context.route('hello', {name: 'World'});
+  // context.route can look up any mounted route by name and params
   res.redirect(helloWorldUrl);
-})
-.description(
-  'Redirect to Hello World route'
-);
+});
