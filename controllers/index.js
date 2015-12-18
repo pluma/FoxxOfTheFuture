0dabeb91ea4498d87c6b8de9e1bd99dbaf486bff
Alan Plum <me@pluma.io>
2015-12-18 11:12:53 +0100

    Explicit session types

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,22 @@
 const context = require('@arangodb/foxx/context'); // magic
 const sessionsMiddleware = require('@arangodb/foxx/sessions'); // replaces activateSessions
+const headerSessions = require('@arangodb/foxx/sessions/header');
+const cookieSessions = require('@arangodb/foxx/sessions/cookie');
 const jwtStorage = require('@arangodb/foxx/sessions/jwt'); // not a Foxx app
 const helloWorldRouter = require('./hello-world');
 const todosRouter = require('./todos');
 
 // Middleware can be used on the context directly, too
 context.use(sessionsMiddleware({
-  sessionStorage: jwtStorage({
+  transport: [
+    headerSessions('x-session-id'), // or just 'header' for defaults
+    cookieSessions({secret: 'keyboardcat'}) // or 'cookie' for defaults
+  ],
+  storage: jwtStorage({
     secret: 'keyboardcat',
-    type: 'HS512'
-  }),
-  header: 'x-session-id',
-  expiry: 60 // minutes
+    type: 'HS512',
+    expiry: 60 // minutes
+  })
 }));
 
 context.use('/todos', todosRouter);
