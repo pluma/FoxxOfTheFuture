6298a237d56511a19b8f73ae3d16b14f0503dc35
Alan Plum <me@pluma.io>
2015-12-18 14:15:27 +0100

    Add more folders

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,16 @@
 const context = require('@arangodb/foxx/context'); // magic
 const sessionsMiddleware = require('@arangodb/foxx/sessions'); // replaces activateSessions
-const headerSessions = require('@arangodb/foxx/sessions/header');
-const cookieSessions = require('@arangodb/foxx/sessions/cookie');
-const jwtStorage = require('@arangodb/foxx/sessions/jwt'); // not a Foxx app
+const headerTransport = require('@arangodb/foxx/sessions/transports/header');
+const cookieTransport = require('@arangodb/foxx/sessions/transports/cookie');
+const jwtStorage = require('@arangodb/foxx/sessions/storages/jwt'); // not a Foxx app
 const helloWorldRouter = require('./hello-world');
 const todosRouter = require('./todos');
 
 // Middleware can be used on the context directly, too
 context.use(sessionsMiddleware({
   transport: [
-    headerSessions('x-session-id'), // or just 'header' for defaults
-    cookieSessions({secret: 'keyboardcat'}) // or 'cookie' for defaults
+    headerTransport('x-session-id'), // or just 'header' for defaults
+    cookieTransport({secret: 'keyboardcat'}) // or 'cookie' for defaults
   ],
   storage: jwtStorage({
     secret: 'keyboardcat',
